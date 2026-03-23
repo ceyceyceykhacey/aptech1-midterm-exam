b6141c4cb946b764fefb31dc0d0b2de79d412cff
lictao_k <lictao_k@ca01.ub.lan>
2026-03-23 11:28:36 +0800

    Created Navigation to implement routing

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav style={styles.nav}>
+      <h2>Student System</h2>
+      <div>
+        <Link to="/home" style={styles.link}>Home</Link>
+        <Link to="/profile" style={styles.link}>Profile</Link>
+        <Link to="/Signup" style={styles.link}>Signup</Link>
+        <Link to="/Success" style={styles.link}>Success</Link>
+        
+      </div>
+    </nav>
+  );
+}
+
+const styles = {
+  nav: {
+    display: "flex",
+    justifyContent: "space-between",
+    padding: "10px",
+    background: "#4f46e5",
+    color: "white"
+  },
+  link: {
+    marginLeft: "15px",
+    color: "white",
+    textDecoration: "none"
+  }
+};
+
+export default Navbar;
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,10 @@
+function Signup() {
+  return (
+    <div style={{padding:"20px"}}>
+      <h1>Signup</h1>
+    
+    </div>
+  );
+}
+
+export default Signup;
